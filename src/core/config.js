5bfe5f66b9db7184229685cf1cc12bd69bec2460
Marcela Mazo <marcela.mazo@correounivalle.edu.co>
2026-04-10 13:56:33 -0500

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -4,7 +4,7 @@ export const CONFIG = {
     BACKEND_URL: 'https://portal.gcf.group/backend/api/proyectos',
     BACKEND_BASE_URL: 'https://portal.gcf.group/backend',
     NEXTCLOUD_URL: 'https://portal.gcf.group',
-    NEXTCLOUD_OAUTH_CLIENT_ID: 'activitytracker-prod-client-id-gcf2026',
-    CLIENT_ID: 'activitytracker-prod-client-id-gcf2026',
+    NEXTCLOUD_OAUTH_CLIENT_ID: 'cQNRp3Wc1QqIRUW5lS93gQriHyYDIi4kSp2jUk5zmBkRlOZTb6wsIZFdECxjfDWm',
+    CLIENT_ID: 'cQNRp3Wc1QqIRUW5lS93gQriHyYDIi4kSp2jUk5zmBkRlOZTb6wsIZFdECxjfDWm',
 };
 
